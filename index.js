10442a0fd4fb94dc3b01deb04472df9482e351c9
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-10-30 02:51:40 -0700

    Express js setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get("", (req, resp) => {
+    resp.send("<h1>Basic node js example</h1>");
+});
+app.get("/about", (req, resp) => {
+    resp.send("<h1>This is about page.</h1>");
+});
+app.get("/contact", (req, resp) => {
+    resp.send("<h1>This is Contact page.</h1>");
+});
+app.listen(5300);
