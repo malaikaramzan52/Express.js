b54335778c6df58d082ea87367d5045a0406a0a0
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-11-10 14:17:57 +0500

    Error handling middleware

---
+++ Errorhandling.js
@@ -0,0 +1,23 @@
+//Error Handling Middleware
+//These middleware are used to handle an error that occurs in routes.
+
+import express from'express';
+const app= express();
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Welcome to the Home Page</h1>");
+})
+app.get("/users",(req,res)=>{
+    res.send1("<h1>Welcome to the Users Page</h1>");
+})
+app.get("/error",(req,res,next)=>{
+    const error = new Error('')
+    error.status=404;
+    next(error);
+})
+function errorhandling(error,req,res,next){
+    res.status(error.status || 500).send("Try after some time, we are facing some issues");
+}
+app.use(errorhandling);
+app.listen(3200);
+
