4dbd911d61bd07dcbd1c9bfb856d39144ba972b7
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-11-11 13:22:50 +0500

    Submit Form and display on template Engine

--- TempEngine.js
+++ TempEngine.js
@@ -1,7 +1,7 @@
 // Template Engine that display data from HTML form
 import express from 'express';
 const app = express();
-
+app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
 app.get('/add-user', (req, res) => {
@@ -9,6 +9,7 @@ app.get('/add-user', (req, res) => {
 });
 
 app.post('/submit-user', (req, res) => {
-    res.render('SubmitUser',req.body);
+    console.log(req.body);
+    res.render('SubmitUser', req.body);
 });
 app.listen(3200);
