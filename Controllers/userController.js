a801c361005c2debc1fb1e8ee2e7c5b01e1dc1d8
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-11-14 13:51:35 +0500

    MVC arcitecture

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,3 +1,7 @@
+//Controller
+//Connecting your Models and your Views
+//Contains in-between logic
+
 import { userList } from '../Models/userModel.js';
 export function handleUsers(req, res) {
     const userData = userList();
--- Models/userModel.js
+++ Models/userModel.js
@@ -1,4 +1,5 @@
 // User model definition
+//Represents your data in your code and work with your data
 export function userList(){
     return ['Anil','Sam','Peter','Tony'];
 }
