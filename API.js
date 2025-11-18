598840050e87a30a425fb44a52f95a4cb1fa3327
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-11-18 14:13:03 +0500

    API examples with Dynamic routes

---
+++ API.js
@@ -0,0 +1,30 @@
+//API Example with dynamic Routes
+import express from 'express';
+import userData from './user.json' with{type:'json'}
+const app=express();
+
+app.get("/",(req,res)=>{
+    console.log(userData);
+    res.send(userData);
+    // res.send("user List api");
+})
+
+app.get("/user/:id",(req,res)=>{
+//for id 
+    // const id=req.params.id;
+    // console.log(id);
+    // res.send(id);
+    // let filterData = userData.filter((user)=> user.id == id);
+    // res.send(filterData);
+//for name 
+    const name = req.params.name;
+    console.log(name);
+    let filterData=userData.filter((user)=> user.username == name);
+    res.send(filterData);
+
+
+
+})
+
+
+app.listen(3000);
