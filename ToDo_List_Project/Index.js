34d81ead50f647bcf87ecaf00bfbe43026f53cce
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-12-23 12:08:37 +0500

    To Do List Project

--- ToDo_List_Project/Index.js
+++ ToDo_List_Project/Index.js
@@ -1,17 +1,20 @@
 import express from "express";
 import path from "path";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 
 const app = express();
 
-//Resolve Path for CSS file
+// Resolve Path for CSS file
 const publicPath = path.resolve("./public");
 app.use(express.static(publicPath));
-//To tell Express.js to use EJS as the templating engine
+
+// Set EJS as templating engine
 app.set("view engine", "ejs");
 
-//Database Setup
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: false }));
 
+// Database Setup
 const dbName = "ToDo-Task";
 const collectionName = "todo-list";
 const url = "mongodb://localhost:27017";
@@ -22,25 +25,21 @@ const connection = async () => {
   return connect.db(dbName).collection(collectionName);
 };
 
-//global middlewares
-app.use(express.urlencoded({ extended: false }));
-
-//Make Routes
-app.get("/", (req, res) => {
-  res.render("list");
+// Routes
+app.get("/", async(req, res) => {
+  // const db = await connection();
+  const collection = await connection();
+  const result = await collection.find().toArray();
+  res.render("list",{result});
 });
+
 app.get("/add", (req, res) => {
   res.render("add");
 });
-app.get("/update", (req, res) => {
-  res.render("update");
-});
-app.get("/update", (req, res) => {
-  res.redirect("/");
-});
-app.get("/add", async (req, res) => {
-  const db = await connection();
-  const collection = db.collection(collectionName);
+
+// Changed to POST so req.body works
+app.post("/add", async (req, res) => {
+  const collection = await connection();
   const result = await collection.insertOne(req.body);
   if (result) {
     res.redirect("/");
@@ -49,4 +48,65 @@ app.get("/add", async (req, res) => {
   }
 });
 
+
+//Delete Route
+app.get("/delete/:id", async (req, res) => {
+  const collection = await connection();
+  const result = await collection.deleteOne({ _id: new ObjectId(req.params.id) });
+  if (result) {
+    res.redirect("/");
+  } else {
+    res.redirect("/Some Error");
+  }
+});
+
+//Populate Data for Update Task
+app.get("/update/:id",async (req, res) => {
+  const collection = await connection();
+  const result = await collection.findOne({ _id: new ObjectId(req.params.id) });
+   if(result){
+    res.render("update",{task: result});
+   }
+   else{
+    res.send("Some Error");
+   }
+});
+// Update Task
+app.post("/update/:id",async (req, res) => {
+  const collection = await connection();
+  const filter = { _id: new ObjectId(req.params.id) };
+  const updatedData = {$set:{title: req.body.title, description: req.body.description}};
+  const result = await collection.updateOne(filter, updatedData);
+   if(result){
+    res.redirect("/");
+   }
+   else{
+    res.send("Some Error");
+   }
+});
+
+// Multi-Delete Route
+
+app.post("/multi-delete",async (req, res) => {
+  const collection = await connection();
+  let selectedTask=[];
+  if(Array.isArray(req.body.selectedTask)){
+    selectedTask =req.body.selectedTask.map((id)=> new ObjectId(id));
+  }
+  else{
+     selectedTask =[new ObjectId(req.body.selectedTask)];
+  }
+  const result = await collection.deleteMany({ _id: { $in: selectedTask } });
+   if(result){
+    res.redirect("/");
+   }
+   else{
+    res.send("Some Error");
+   }
+});
+
+
+// Removed duplicate /update GET route that redirected
+
+// Start server
 app.listen(8080);
